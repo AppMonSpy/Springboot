40acced02dd028d5c360e807bc5ad66bbe608654
nikhil-govind <54119683+nikhil-govind@users.noreply.github.com>
2020-06-18 17:02:43 -0700

    Share market call
    Removed the dead method

--- Grafana/src/main/java/com/appspy/udiscover/ShareValuePrometheus.java
+++ Grafana/src/main/java/com/appspy/udiscover/ShareValuePrometheus.java
@@ -73,34 +73,4 @@ public class ShareValuePrometheus {
 			e.printStackTrace();
 		}
 	}
-	public static void main1(String[] args) {
-		Counter counter = Counter.build().namespace("java").name("my_counter").help("This is my counter").register();
-		Gauge gauge = Gauge.build().namespace("java").name("my_gauge").help("This is my gauge").register();
-		Histogram histogram = Histogram.build().namespace("java").name("my_histogram").help("This is my histogram").register();
-		Summary summary = Summary.build().namespace("java").name("my_summary").help("This is my summary").register();
-
-		Thread bgThread = new Thread(() -> {
-			while (true) {
-				try {
-					counter.inc(rand(0, 5));
-					gauge.set(rand(-5, 10));
-					histogram.observe(rand(0, 5));
-					summary.observe(rand(0, 5));
-
-
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		bgThread.start();
-
-		try {
-
-			HTTPServer server = new HTTPServer(8080);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 }
